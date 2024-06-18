75a772f5d6bdefb822de8f6828522e8e9fd1baa8
hyungupark <hyungu.1215.park@gmail.com>
2024-06-19 01:24:09 +0900

    try "order" order the results

--- lib/main.dart
+++ lib/main.dart
@@ -234,6 +234,14 @@ class _MyHomePageState extends State<MyHomePage> {
     print(data);
   }
 
+  void orderTheResults() async {
+    final data = await supabase
+        .from("countries")
+        .select("id, name")
+        .order("id", ascending: false);
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -247,7 +255,7 @@ class _MyHomePageState extends State<MyHomePage> {
         }),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: returnDataAfterInserting,
+        onPressed: orderTheResults,
       ),
     );
   }
