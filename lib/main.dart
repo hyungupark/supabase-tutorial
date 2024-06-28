48d86a095f8cec35afa14bbffb7552d43afe4cc1
hyungupark <hyungu.1215.park@gmail.com>
2024-06-28 22:33:43 +0900

    try "auth.signInAnonymously" create an anonymous user

--- lib/main.dart
+++ lib/main.dart
@@ -102,56 +102,61 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void selectEqualTo() async {
-    final data = await supabase.from("countries").select().eq("name", "Korea");
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select().eq("name", "Korea");
     print(data);
   }
 
   void selectNotEqualTo() async {
-    final data =
+    final List<Map<String, dynamic>> data =
         await supabase.from("countries").select("name").neq("name", "China");
     print(data);
   }
 
   void selectGreaterThan() async {
-    final data = await supabase.from("countries").select().gt("id", 2);
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select().gt("id", 2);
     print(data);
   }
 
   void selectGreaterThanOrEqualTo() async {
-    final data = await supabase.from("countries").select().gte("id", 2);
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select().gte("id", 2);
     print(data);
   }
 
   void selectLessThan() async {
-    final data = await supabase.from("countries").select().lt("id", 2);
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select().lt("id", 2);
     print(data);
   }
 
   void selectLessThanOrEqualTo() async {
-    final data = await supabase.from("countries").select().lte("id", 2);
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select().lte("id", 2);
     print(data);
   }
 
   void selectMatchPattern() async {
-    final data =
+    final List<Map<String, dynamic>> data =
         await supabase.from("countries").select().like("name", "%rea%");
     print(data);
   }
 
   void selectMatchCaseInsensitivePattern() async {
-    final data =
+    final List<Map<String, dynamic>> data =
         await supabase.from("countries").select().ilike("name", "%REA%");
     print(data);
   }
 
   void selectIsValue() async {
-    final data =
+    final List<Map<String, dynamic>> data =
         await supabase.from("countries").select().isFilter("name", null);
     print(data);
   }
 
   void selectInArray() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("countries")
         .select()
         .inFilter("name", ["Korea", "Japan"]);
@@ -159,7 +164,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void containsEveryElementInValue() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("issues")
         .select()
         .contains("tags", ["is:open", "priority:low"]);
@@ -167,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void containedByValue() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("classes")
         .select("name")
         .containedBy("days", ["monday", "tuesday", "wednesday", "friday"]);
@@ -175,7 +180,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void greaterThanARange() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("reservations")
         .select()
         .rangeGt("during", "[2000-01-02 08:00, 2000-01-02 09:00)");
@@ -183,7 +188,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void greaterThanOrEqualToARange() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("reservations")
         .select()
         .rangeGte("during", "[2000-01-02 08:30, 2000-01-02 09:30)");
@@ -191,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void lessThanARange() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("reservations")
         .select()
         .rangeLt("during", "[2000-01-01 15:00, 2000-01-01 16:00)");
@@ -199,7 +204,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void lessThanOrEqualToARange() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("reservations")
         .select()
         .rangeLte("during", "[2000-01-01 15:00, 2000-01-01 16:00)");
@@ -207,7 +212,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void mutuallyExclusiveToARange() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("reservations")
         .select()
         .rangeAdjacent("during", "[2000-01-01 12:00, 2000-01-01 13:00)");
@@ -215,7 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void withACommonElement() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("issues")
         .select("title")
         .overlaps("tags", ["is:closed", "severity:high"]);
@@ -223,7 +228,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void matchAString() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("texts")
         .select("content")
         .textSearch("content", "\"eggs\" & \"ham\"", config: "english");
@@ -231,7 +236,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void matchAnAssociatedValue() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("countries")
         .select()
         .match({"id": 4, "name": "Korea"});
@@ -239,13 +244,13 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void dontMatchTheFilter() async {
-    final data =
+    final List<Map<String, dynamic>> data =
         await supabase.from("countries").select().not("name", "is", null);
     print(data);
   }
 
   void matchAtLeastOneFilter() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("countries")
         .select("name")
         .or("id.eq.2,name.eq.Algeria");
@@ -253,7 +258,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void matchTheFilter() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("countries")
         .select()
         .filter("name", "in", "(\"Korea\",\"Japan\")");
@@ -261,14 +266,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void returnDataAfterInserting() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("countries")
         .upsert({"id": 1, "name": "Algeria"}).select();
     print(data);
   }
 
   void orderTheResults() async {
-    final data = await supabase
+    final List<Map<String, dynamic>> data = await supabase
         .from("countries")
         .select("id, name")
         .order("id", ascending: false);
@@ -276,23 +281,25 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void limitTheNumberOfRowsReturned() async {
-    final data = await supabase.from("countries").select("name").limit(1);
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select("name").limit(1);
     print(data);
   }
 
   void limitTheQueryToARange() async {
-    final data = await supabase.from("countries").select("name").range(0, 1);
+    final List<Map<String, dynamic>> data =
+        await supabase.from("countries").select("name").range(0, 1);
     print(data);
   }
 
   void retrieveOneRowOfData() async {
-    final data =
+    final Map<String, dynamic> data =
         await supabase.from("countries").select("name").limit(1).single();
     print(data);
   }
 
   void retrieveZeroOrOneRowOfData() async {
-    final data = await supabase
+    final Map<String, dynamic>? data = await supabase
         .from("countries")
         .select()
         .eq("name", "Singapore")
@@ -301,12 +308,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void retrieveAsACSV() async {
-    final data = await supabase.from("countries").select().csv();
+    final String data = await supabase.from("countries").select().csv();
     print(data);
   }
 
   void usingExplain() async {
-    final data = await supabase.from("countries").select().explain();
+    final String data = await supabase.from("countries").select().explain();
     print(data);
   }
 
@@ -321,11 +328,16 @@ class _MyHomePageState extends State<MyHomePage> {
     print(user);
   }
 
+  void createAnAnonymousUser() async {
+    final AuthResponse data = await supabase.auth.signInAnonymously();
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: createANewUser,
+        onPressed: createAnAnonymousUser,
       ),
     );
   }
